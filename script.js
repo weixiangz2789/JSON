30b1a3c3619ff3583745cd5c393e1f376f808406
Wei <weixiangz2789@nycstudents.net>
2023-12-05 21:34:25 -0500

    w

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ async function getData(link) {
   try {
     let response = await fetch(link);
     let data = await response.json();
-    console.log(data);
+    // console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-function appendPosts(obj) {
+async function appendPosts(link) {
   const p = document.createElement("p");
-  p.textContent = obj[0].name;
+  const data = await getData(link);
+  p.textContent = data[0].name;
   document.body.append(p);
 }
-appendPosts(getData("https://jsonplaceholder.typicode.com/users"));
+appendPosts("https://jsonplaceholder.typicode.com/users");
