4295403a4792b5cfe72d30df9871e48d8853ddd8
Wei <weixiangz2789@nycstudents.net>
2023-12-07 20:29:28 -0500

    w

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 async function getData(link) {
   try {
-    let response = await fetch(link);
-    let data = await response.json();
-    // console.log(data);
+    let response = await fetch(link); // Fetch data from our link
+    let data = await response.json(); // Parses json and converts it to js object
+    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
@@ -10,9 +10,36 @@ async function getData(link) {
 }
 
 async function appendPosts(link) {
-  const p = document.createElement("p");
   const data = await getData(link);
-  p.textContent = data[0].name;
-  document.body.append(p);
+  const tableContainer = document.createElement("div");
+  tableContainer.classList.add("table-container");
+
+  const table = document.createElement("table");
+
+  // Create table header
+  const headerRow = document.createElement("tr");
+  const headerFields = ["Name", "Username", "Email", "Website"];
+  headerFields.forEach((field) => {
+    const th = document.createElement("th");
+    th.textContent = field;
+    headerRow.appendChild(th);
+  });
+  table.appendChild(headerRow);
+
+  // Create table rows
+  data.forEach((item) => {
+    const row = document.createElement("tr");
+    const fields = ["name", "username", "email", "website"];
+    fields.forEach((field) => {
+      const cell = document.createElement("td");
+      cell.textContent = item[field];
+      row.appendChild(cell);
+    });
+    table.appendChild(row);
+  });
+
+  tableContainer.appendChild(table);
+  document.body.appendChild(tableContainer);
 }
+
 appendPosts("https://jsonplaceholder.typicode.com/users");
